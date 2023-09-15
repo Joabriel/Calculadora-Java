e09a29b5754660aa48e85e7c8844464428d5bfbb
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-09-15 01:42:24 -0300

    Hice una suma de dos valores introducidos por el usuario.

--- Calc.java
+++ Calc.java
@@ -1,5 +1,15 @@
+import java.util.Scanner();
 class Calc{
     public static void main(String[]args){
-        System.out.print("Hola!");
+        Scanner reader = new Scanner(System.in);
+        int num1, num2, resultado=0;
+        System.out.println("Suma: ");
+        num1 = reader.nextInt();
+        num2 = reader.nextInt();
+
+        resultado = num1+num2;
+        System.out.println("El Resultado es: "+resultado);
+
+        reader.close();
     }
 }
